571f4fc4d20102925dabe3cc90cefdb55e413aba
priyaa0961 <priyaa0k@gmail.com>
2026-07-05 17:49:16 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+from groq import Groq
+
+client = Groq(
+    api_key="YOUR API KEY"
+)
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {
+            "role": "user",
+            "content": "Say hello"
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
